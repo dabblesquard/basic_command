b6d9e76811096fb183506cf58c86aa9245e650e8
dabblesquard <130145123+dabblesquard@users.noreply.github.com>
2025-08-01 20:27:33 +0600

    Add files via upload

---
+++ package.py
@@ -0,0 +1,81 @@
+
+# Logo 
+
+import os 
+import sys 
+import time
+
+
+os.system("clear")
+
+print("\n\n\n\n")
+
+ab="           \033[1;33mSystem loding...."
+
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+
+print("\n\n\n\n")
+
+
+os.system("clear")
+
+time.sleep(2)
+print("\n\n")
+
+ab="         \033[1;32mSuccessfully Loaded.!\n"
+
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+name=input("         \033[1;34mName:")
+
+ab="         \033[1;31mHey "+name+", Be Ethical.."
+
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+
+print("\n\n")
+
+
+time.sleep(2)
+
+os.system("clear")
+
+print(""" \033[1;31m
+
+
+   _____  .__                 
+  /  _  \\ |  | _____  ___  ___
+ /  /_\\  \\|  | \\__  \\ \\  \\/  /
+/    |    \\  |__/ __ \\_>    < 
+\\____|__  /____(____  /__/\\_ \\
+        \\/          \\/      \\/
+        
+
+\033[1;33m========================================
+\033[1;32m Owner: Alax Mahmud
+ Github: Comming Soon
+ Facebook: Alax Hridoy
+Use this tools only for educational purposes
+\033[1;33m========================================            
+        
+        \033[0m
+
+""")
+
+
+import os
+
+os.system("pkg up")
+os.system("\033[0;34mpkg install git")
+os.system("\033[0;31mpkg install ruby")
+os.system("\033[0;32m gem install lolcat")
+os.system("\033[0;33m pip install request")
+
+print("\033[1;32mAll Package Install Successfull")
